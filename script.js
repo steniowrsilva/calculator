cd6f76bc8d6b3038661851bf74d0112d14ca230f
steniowrsilva <steniowoney@gmail.com>
2023-02-28 20:30:37 -0300

    correct result's method reduce

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const add = function(a,b){
-    return a+b;
+const add = function(sum, number){
+    return sum+number;
 }
 
-const subtract = function(a,b){
-    return a-b;
+const subtract = function(difference, number){
+    return difference-number;
 }
 
-const multiply = function(a,b){
-    return a*b;
+const multiply = function(product, number){
+    return product*number;
 }
 
-const divide = function(a,b){
-    return a/b;
+const divide = function(ratio, number){
+    return ratio/number;
 }
 
-const operate = function (operator, num1, num2) {
-    return operator(num1,num2);
+// return numbers.reduce((sum, number) => {return add(sum, number)})
+
+const operate = function (operator, result) {
+    switch (operator) {
+        case '+':
+            return result.reduce((sum, number) => add(sum, number), 0);
+
+        case '-':
+            return result.reduce((difference, number) => subtract(difference, number), result[0]);
+
+        case 'x':
+            return result.reduce((product, number) => multiply(product, number), 1);
+
+        case '÷':
+            return result.reduce((ratio, number) => divide(ratio, number), result[0]);
+    }
 }
 
 // KEYPAD
@@ -32,57 +46,46 @@ const nine = document.querySelector('.nine');
 
 // DISPLAY
 const display = document.querySelector('#display');
-let displayText = '';
 
 // OPERATORS
 const clear = document.querySelector('#clear');
 
 // NUMBERS EVENTS
-zero.addEventListener('click', e => {
+zero.addEventListener('click', () => {
     display.textContent==='0' ? display.textContent = '0': display.textContent += '0';
-    displayText += display.textContent;
 });
-one.addEventListener('click', e => {
+one.addEventListener('click', () => {
     display.textContent==='0' ? display.textContent = '1': display.textContent += '1';
-    displayText += display.textContent;
 });
 two.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '2': display.textContent += '2';
-    displayText = display.textContent;
 });
 three.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '3': display.textContent += '3';
-    displayText = display.textContent;
 });
 four.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '4': display.textContent += '4';
-    displayText = display.textContent;
 });
 five.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '5': display.textContent += '5';
-    displayText = display.textContent;
 });
 six.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '6': display.textContent += '6';
-    displayText = display.textContent;
 });
 seven.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '7': display.textContent += '7';
-    displayText = display.textContent;
 });
 eight.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '8': display.textContent += '8';
-    displayText = display.textContent;
 });
 nine.addEventListener('click', e => {
     display.textContent==='0' ? display.textContent = '9': display.textContent += '9';
-    displayText = display.textContent;
 });
 
 // CLEAR EVENT
 clear.addEventListener('click', e => {
     display.textContent = '0';
-    displayText = display.textContent;
+    result=[display.textContent];
 });
 
 // OPERATORS
@@ -90,8 +93,40 @@ const addButton = document.querySelector('.add');
 const subtractButton = document.querySelector('.subtract');
 const multiplyButton = document.querySelector('.multiply');
 const divideButton = document.querySelector('.divide');
+const equalButton = document.querySelector('.equal');
 
 // OPERATORS EVENTS
-addButton.addEventListener('click', e => {
-    let fistNumber = parseFloat(display.textContent);
+let result=[];
+let operator;
+addButton.addEventListener('click', () => {
+    result.push(parseFloat(display.textContent));
+    operator = '+';
+    result=[operate(operator,result)];
+    display.textContent = '0';    
+});
+
+subtractButton.addEventListener('click', ()=> {
+    result.push(parseFloat(display.textContent));
+    operator = '-';
+    result=[operate(operator,result)];
+    display.textContent = '0';
+})
+
+multiplyButton.addEventListener('click', ()=> {
+    result.push(parseFloat(display.textContent));
+    operator = 'x';
+    result=[operate(operator,result)];
+    display.textContent = '0';
+})
+
+divideButton.addEventListener('click', ()=> {
+    result.push(parseFloat(display.textContent));
+    operator = '÷';
+    result=[operate(operator,result)];
+    display.textContent = '0';
+})
+
+equalButton.addEventListener('click', () => {
+    display.textContent=result[0];
+    result=[];
 });
